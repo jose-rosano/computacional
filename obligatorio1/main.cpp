874ce508f40270ed7d67aa4979a54c293fd3a951
jose-rosano <e.funtoimagine18@go.ugr.es>
2022-03-21 19:56:56 +0100

    Clase 22.03,21

---
+++ obligatorio1/main.cpp
@@ -0,0 +1,15 @@
+// Obligatorio 1: Simular la Dinámica del Sistema Solar
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  float h=0, t=0; 
+  float r[2][10];
+
+
+//  cout << h << endl;
+  return 0;
+}
