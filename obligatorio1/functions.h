bc2fa7747ce761573b379b339fc6c7b843d9a06a
Jose Rosano <e.funtoimagine18@go.ugr.es>
2022-03-23 23:54:25 +0100

    Terminando funciones base

--- obligatorio1/functions.h
+++ obligatorio1/functions.h
@@ -10,17 +10,32 @@ using namespace std;
 void dif (float x[], float y[], float z[]);
 void mostrar(float x[]);
 
-//Función diferencia de vectores
+//Función Diferencia de vectores
 void dif (float x[], float y[], float z[]){
   for(int i=0; i<2; i++)
     z[i] = x[i] - y[i];
 }
 
-//Función Mostrar
+//Función Mostrar vectores
 void mostrar(float x[]){
   for(int i=0; i<2; i++){
     cout.width(7);
     cout << x[i] << "  ";
   }
   cout << endl;
+}
+
+//Función Leer datos iniciales
+void LeerData(float x[]){
+  ifstream fich;
+  
+  fich.open("datos.txt");
+  if(!fich.is_open())
+    cout << "Error al abrir el fichero";
+  else 
+    while(!fich.eof()){
+        for(int j=0; j<2; j++)
+          fich >> x[j];
+    }
+  fich.close();
 }
--- obligatorio1/main.cpp
+++ obligatorio1/main.cpp
@@ -4,33 +4,24 @@
 #include <cmath>
 #include <fstream>
 #include "functions.h"
+
+#define G 0.001
 using namespace std;
 
 void dif (float x[], float y[], float z[]);
 void mostrar(float x[]);
-
-void LeerData(float x[][], float y[][], float z[][]){
-  ifstream fich;
-  
-  fich.open("datos.txt");
-  if(!fich.is_open())
-    cout << "Error al abrir el fichero";
-  else 
-    while(!fich.eof()){
-
-    }
-  fich.close();
-}
+void LeerData(float x[]);
 
 int main(){
   int N=3; //Numero de planetas
   float h=0, t=0;
-  float r[N][2]={{0,1},{0,2},{0,3}},
-        v[N][2]={{0,4},{0,5},{0,6}},
-        a[N][2]={{0,0},{0,0},{0,0}};
+  float r[N][2];
+//        v[N][2]={{0,4},{0,5},{0,6}},
+//        a[N][2]={{0,0},{0,0},{0,0}};
 
-  dif(r[1],r[2],a[0]);
-  mostrar(a[0]);
+//  dif(r[1],r[2],a[0]);
+  LeerData(r[0]);
+  mostrar(r[0]);
 
   return 0;
 }
