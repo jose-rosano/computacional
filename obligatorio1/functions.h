22f50618c968bdd90fc3f2dc672a18a9eb0abda8
Jose Rosano <e.funtoimagine18@go.ugr.es>
2022-03-24 17:37:05 +0100

    22.03.24_Lectura y Escritura de Datos, aceleración

--- obligatorio1/functions.h
+++ obligatorio1/functions.h
@@ -5,19 +5,46 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+
+#define N 3 //Número planetas
 using namespace std;
 
-void dif (float x[], float y[], float z[]);
-void mostrar(float x[]);
+void Sum(float x[], float ret[]); //'ret' de return (la variable que se ve alterada) 
+void Mult(float n, float ret[]);
+void Dif(float x[], float y[], float ret[]);
+float Norm(float x[]);
+void Mostrar(float x[]);
+void Init(float ret[][2]);
+
+void LeerData(float x[][2], float y[][2], float z[]);
+void Acel_t(float x[][2], float z[], float ret[][2]);
+
+
+//Función Suma del 1er vector en el 2o
+void Sum(float x[], float ret[]){
+  for(int i=0; i<2; i++)
+    ret[i] = x[i] + ret[i];
+}
+
+//Función Multiplicar (vector) por escalar
+void Mult(float n, float ret[]){
+  for(int i=0; i<2; i++)
+    ret[i] = n * ret[i];
+}
 
 //Función Diferencia de vectores
-void dif (float x[], float y[], float z[]){
+void Dif(float x[], float y[], float ret[]){
   for(int i=0; i<2; i++)
-    z[i] = x[i] - y[i];
+    ret[i] = x[i] - y[i];
+}
+
+//Función Norma de un vector
+float Norm(float x[]){
+  return sqrt(x[0]*x[0] + x[1]*x[1]);
 }
 
 //Función Mostrar vectores
-void mostrar(float x[]){
+void Mostrar(float x[]){
   for(int i=0; i<2; i++){
     cout.width(7);
     cout << x[i] << "  ";
@@ -25,8 +52,15 @@ void mostrar(float x[]){
   cout << endl;
 }
 
+//Inicializar Matriz a Cero
+void Init(float ret[][2]){
+  for(int i=0; i<N; i++)
+    for(int j=0; j<2; j++)
+      ret[i][j]=0;
+}
+
 //Función Leer datos iniciales
-void LeerData(float x[]){
+void LeerData(float x[][2], float y[][2], float z[]){
   ifstream fich;
   
   fich.open("datos.txt");
@@ -34,8 +68,35 @@ void LeerData(float x[]){
     cout << "Error al abrir el fichero";
   else 
     while(!fich.eof()){
-        for(int j=0; j<2; j++)
-          fich >> x[j];
+      //Lee las posiciones
+      for(int i=0; i<N; i++)
+        for (int j=0; j<2; j++)
+          fich >> x[i][j];
+      
+      //Lee las velocidades
+      for(int i=0; i<N; i++)
+        for (int j=0; j<2; j++)
+          fich >> y[i][j];
+
+      //Lee las masas
+      for(int i=0; i<N; i++)
+        fich >> z[i];
     }
   fich.close();
+}
+
+//Función Aceleración inicial
+void Acel_t(float x[][2], float z[], float ret[][2]){
+  float aux=0, aux_arr[N][2];
+
+  for(int i=0; i<N; i++){
+    Init(aux_arr);
+    for(int j=0; j<N; j++)
+      if(j!=i){
+        Dif(x[i],x[j],aux_arr[j]); 
+        aux = -z[j]/pow(Norm(aux_arr[j]),3);
+        Mult(aux,aux_arr[j]);
+        Sum(aux_arr[j],ret[i]);
+      }
+  }
 }
--- obligatorio1/main.cpp
+++ obligatorio1/main.cpp
@@ -1,27 +1,51 @@
-// Obligatorio 1: Simular la Dinámica del Sistema Solar
+//****************************************************
+//***_ OBL1: Simular la Dinámica del Sistema Solar_***
+//****************************************************
 
 #include <iostream>
 #include <cmath>
 #include <fstream>
 #include "functions.h"
 
-#define G 0.001
+#define N 3 //Número planetas
 using namespace std;
 
-void dif (float x[], float y[], float z[]);
-void mostrar(float x[]);
-void LeerData(float x[]);
+void Sum(float x[], float ret[]);
+void Mult(float n, float ret[]);
+void Dif(float x[], float y[], float ret[]);
+float Norm(float x[]);
+void Mostrar(float x[]);
+void Init(float ret[][2]);
+
+void LeerData(float x[][2], float y[][2], float z[]);
+void Acel_t(float x[][2], float z[], float ret[][2]);
+
+void ExportData(float x[][2]){
+  ofstream fich;
+
+  fich.open("planets_data.dat");
+  //Lee las posiciones
+  for(int i=0; i<N; i++)
+    fich << x[i][0] << "  " << x[i][1] << endl;
+
+  fich.close();
+}
 
 int main(){
-  int N=3; //Numero de planetas
   float h=0, t=0;
-  float r[N][2];
-//        v[N][2]={{0,4},{0,5},{0,6}},
-//        a[N][2]={{0,0},{0,0},{0,0}};
+  float r[N][2], v[N][2], m[N], a[N][2]; //Radio orbital, velocidad orbital, masa y aceleración
 
-//  dif(r[1],r[2],a[0]);
-  LeerData(r[0]);
-  mostrar(r[0]);
+  //a=0 y lectura de datos iniciales (CON REESCALAMIENTO)
+  Init(a);
+  LeerData(r,v,m);
 
+  //Obtención de la aceleración inicial y Escritura en fichero
+  Acel_t(r,m,a);
+  ExportData(a);
+
+
+
+//Mostrar(r[0]);
+//  cout << v[0][1];
   return 0;
 }
