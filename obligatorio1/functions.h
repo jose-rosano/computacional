cee1d2cb71df05d38809646a3a6f8fdadfe119a5
Jose Rosano <e.funtoimagine18@go.ugr.es>
2022-03-29 00:20:20 +0200

    22.03.28_Exportar Posiciones

--- obligatorio1/functions.h
+++ obligatorio1/functions.h
@@ -5,49 +5,19 @@
 #include <cmath>
 #include <fstream>
 
-#define N 3 //Número planetas
+#define N 10 //Número planetas
 #define c 149.6e9 //Distancia Tierra-Sol (en metros)
 #define cte 3.35695e-5 // =sqrt(c/GM)
 #define M 1.989e30 //Masa del Sol (en kg)
 using namespace std;
 
-//Funciones soporte de las posteriores
-void Sum(float x[], float ret[]); //'ret' de return (la variable que se ve alterada) 
-void Mult(float n, float ret[]);
-void Dif(float x[], float y[], float ret[]);
-float Norm(float x[]);
-void Mostrar(float x[]);
+void Mostrar(float x[]); //Usada para hacer pruebas de funcionamiento
 
 void Init(float ret[][2]);
 void LeerData(float x[][2], float y[][2], float z[]);
 void Rescaling(float x[][2], float y[][2], float z[]);
-void Acel_t(float x[][2], float z[], float ret[][2]);
+void New_a(float x[][2], float z[], float ret[][2]);
 
-void ExportData(float x[][2]);
-
-
-//Función Suma del 1er vector en el 2o
-void Sum(float x[], float ret[]){
-  for(int i=0; i<2; i++)
-    ret[i] = x[i] + ret[i];
-}
-
-//Función Multiplicar (vector) por escalar
-void Mult(float n, float ret[]){
-  for(int i=0; i<2; i++)
-    ret[i] = n * ret[i];
-}
-
-//Función Diferencia de vectores
-void Dif(float x[], float y[], float ret[]){
-  for(int i=0; i<2; i++)
-    ret[i] = x[i] - y[i];
-}
-
-//Función Norma de un vector
-float Norm(float x[]){
-  return sqrt(x[0]*x[0] + x[1]*x[1]);
-}
 
 //Función Mostrar vectores
 void Mostrar(float x[]){
@@ -104,30 +74,23 @@ void Rescaling(float x[][2], float y[][2], float z[]){
   }
 }
 
-//Función Aceleración inicial
-void Acel_t(float x[][2], float z[], float ret[][2]){
+//Función Aceleración
+void New_a(float x[][2], float z[], float ret[][2]){
   float aux=0, aux_arr[N][2];
-
+  
+  Init(ret);
   for(int i=0; i<N; i++){
-    Init(aux_arr);
+    Init(aux_arr); 
     for(int j=0; j<N; j++)
       if(j!=i){
-        Dif(x[i],x[j],aux_arr[j]); 
-        aux = -z[j]/pow(Norm(aux_arr[j]),3);
-        Mult(aux,aux_arr[j]);
-        Sum(aux_arr[j],ret[i]);
+        for(int k=0; k<2; k++)
+          aux_arr[j][k] = x[i][k] - x[j][k];
+
+        aux=-z[j]/pow(aux_arr[j][0]*aux_arr[j][0] + aux_arr[j][1]*aux_arr[j][1],1.5);
+        for(int k=0; k<2; k++){
+          aux_arr[j][k] *= aux;
+          ret[i][k] += aux_arr[j][k];
+        }
       }
   }
-}
-
-//Función Exportar Datos, re-reescalados
-void ExportData(float x[][2]){
-  ofstream fich;  
-
-  fich.open("planets_data.dat");
-  //Escribe las posiciones, re-reescaladas
-  for(int i=0; i<N; i++)
-    fich << x[i][0]*c << "   " << x[i][1]*c << endl;
-
-  fich.close();
 }
--- obligatorio1/main.cpp
+++ obligatorio1/main.cpp
@@ -6,45 +6,53 @@
 #include <fstream>
 #include "functions.h"
 
-#define N 3 //Número planetas
+#define N 10 //Número planetas
 #define c 149.6e9 //Distancia Tierra-Sol (en metros)
 #define cte 3.35695e-5 // =sqrt(c/GM)
 #define M 1.989e30 //Masa del Sol (en kg)
 using namespace std;
 
-//void Sum(float x[], float ret[]); //QUIZÁ NO HAGAN MUCHA FALTA
-//void Mult(float n, float ret[]);
-//void Dif(float x[], float y[], float ret[]);
-//float Norm(float x[]);
-//void Mostrar(float x[]);
-
-void Init(float ret[][2]);
-void LeerData(float x[][2], float y[][2], float z[]);
-void Rescaling(float x[][2], float y[][2], float z[]);
-void Acel_t(float x[][2], float z[], float ret[][2]);
-
-void ExportData(float x[][2]);
 
 int main(){
   float h=0.01, t=0; //h elegido porque t'~0.0172t
   float r[N][2], v[N][2], m[N], a[N][2], w[N][2]; //Radio orbital, velocidad orbital, masa, aceleración y w
+  ofstream fich; 
 
   //a=0, w=0, Lectura de datos iniciales y Reescalamiento
   Init(a); Init(w);
   LeerData(r,v,m);
   Rescaling(r,v,m);
 
-  //Algoritmo de Verlet
-  Acel_t(r,m,a);
-  //New_r();
-  //New_w();
-  //New_a();
-  //New_v();
-  //t += h;
+  New_a(r,m,a); //1ª aceleración
 
-  //Escritura en fichero
-  ExportData(r);
+  //Apertura de fichero con las posiciones
+  fich.open("planets_data.dat");
+  for(int i=0; i<N; i++)
+    fich << r[i][0] << ",   " << r[i][1] << endl;
+  
+  fich << endl;
+  //Algoritmo de Verlet 
+  do{
+    for(int i=0; i<N; i++)
+      for(int j=0; j<2; j++){
+        r[i][j] = r[i][j] + h*v[i][j] + h*h/2*a[i][j]; //Nueva r
+        w[i][j] = v[i][j] + h/2*a[i][j]; //Nueva w
+      }
 
+    New_a(r,m,a); //Nueva a
 
+    for(int i=0; i<N; i++)
+      for(int j=0; j<2; j++)
+        v[i][j] = w[i][j] + h/2*a[i][j]; //Nueva v
+
+    //Escritura de las posiciones en fichero, re-reescaladas
+    for(int i=0; i<N; i++)
+      fich << r[i][0] << ",   " << r[i][1] << endl;
+
+    fich << endl;
+    t += h;
+  } while(t<=5);
+
+  fich.close();
   return 0;
 }
