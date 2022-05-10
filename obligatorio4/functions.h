67497654d8130caddf4e965f8ec4c6b61d084ed7
Jose Rosano <e.funtoimagine18@go.ugr.es>
2022-05-10 13:57:48 +0200

    22.5.10_OBL4 Leer, reescalar cond inic + exportar

--- obligatorio4/functions.h
+++ obligatorio4/functions.h
@@ -7,15 +7,14 @@
 
 #define N 4 // Número de ecuaciones diferenciales
 #define G 6.6738e-11 // Cte Gravitación Universal
+#define dTL 3.844e8 // Distancia Tierra-Luna (m)
+#define w 2.60146e-9 // vL = 1.0 (km/s)
 using namespace std;
 
 void Init(float ret[][2]);
 void InitV(float ret[]);
-void LeerData(float x[][2], float y[][2], float z[]);
-void Rescaling(float x[][2], float y[][2], float z[]);
-void New_a(float x[][2], float z[], float ret[][2]);
-void New_L_E(float x[][2], float y[][2], float z[], float &cte1, float &cte2);
-void CalcT(float x[][2], float z[], float a);
+void LeerReescalarData(float y[], float r, float phi, float v, float theta);
+void ExportData(ofstream &fich, float y[], float t);
 
 //*************************
 //Inicializar Matriz a Cero
@@ -31,79 +30,20 @@ void InitV(float ret[]){
     ret[j]=0;
 }
 
-//Función Leer datos iniciales
-void LeerData(float y[]){
-  ifstream fich;
-  float aux[N];
-  
-  fich.open("datos.txt");
-  if(!fich.is_open())
-    cout << "Error al abrir el fichero";
-  else 
-    while(!fich.eof()){
-      // Lee los datos del fichero (r,phi,v,theta)
-      for(int i=0; i<N; i++)
-        fich >> aux[i];
+//Función Leer y Reescalar Condiciones Iniciales
+void LeerReescalarData(float y[], float r, float phi, float v, float theta){
+  float vhat= v/dTL, aux=3.1415926535/180;
 
-      // Crea las cond iniciales
-      y[0] = aux[0]; 
-      y[1] = aux[1];
-      y[2] = aux
-    }
-  fich.close();
+  y[0] = r/dTL;
+  y[1] = phi*aux; // Pasa a radianes
+  y[2] = vhat * cos(aux*(theta -phi));
+  y[3] = y[0] * vhat * sin(aux*(theta -phi));
 }
 
-//Función Reescalamiento de Datos iniciales
-void Rescaling(float y[]){
-  
-}
-
-//Función Aceleración
-void New_a(float x[][2], float z[], float ret[][2]){
-  float aux=0, aux_arr[N][2];
-  
-  Init(ret);
-  for(int i=0; i<N; i++){
-    Init(aux_arr); 
-    for(int j=0; j<N; j++)
-      if(j!=i){
-        for(int k=0; k<2; k++)
-          aux_arr[j][k] = x[i][k] - x[j][k];
-
-        aux=-z[j]/pow(aux_arr[j][0]*aux_arr[j][0] + aux_arr[j][1]*aux_arr[j][1],1.5);
-        for(int k=0; k<2; k++){
-          aux_arr[j][k] *= aux;
-          ret[i][k] += aux_arr[j][k];
-        }
-      }
-  }
-}
-
-//Función Momento Angular y Energía
-void New_L_E(float x[][2], float y[][2], float z[], float &cte1, float &cte2){
-  cte1=0;
-  cte2=0;
-
-  for(int i=0; i<N; i++){
-    cte1 += z[i] * (x[i][0]*y[i][1] - x[i][1]*y[i][0]); //Mom Ang Total
-
-    cte2 += 0.5*z[i] * (y[i][0]*y[i][0] + y[i][1]*y[i][1]);
-    for(int j=0; j<N; j++)
-      if(j!=i){
-        cte2 -= G*z[i]*z[j]*pow( (x[i][0]-x[j][0])*(x[i][0]-x[j][0]) + (x[i][1]-x[j][1])*(x[i][1]-x[j][1]), -0.5); 
-      }
-  }
-}
-
-//Función Calcular Periodo de los planetas
-void CalcT(float x[][2], float z[], float a){
-  if(a>1.3 && a<12) //Filtro conteos iniciales
-    for(int i=1; i<=4; i++) //Solo planetas Rocosos
-      if(x[i][0]>0 && x[i][1]>0 && z[i]==0 && x[i][1]<0.02) //Filtros
-        z[i] = a;
-
-  if(a>74) //Sabemos que en el intervalo [12,74] no hay lecturas de periodos (esto da + velocidad)
-    for(int i=5; i<N; i++) //Solo planetas Gaseosos
-      if(x[i][0]>0 && x[i][1]>0 && z[i]==0 && x[i][1]<0.05) //Filtros
-        z[i] = a;
+//Función Escritura en Fichero
+void ExportData(ofstream &fich, float y[], float t){
+  fich << 0 << ",   " << 0 << endl;
+  fich << cos(w*t) << ",   " << sin(w*t) << endl; // No se multiplica dTL por el cambio de variable
+  fich << y[0]*cos(y[1]) << ",   " << y[0]*sin(y[1]) << endl;
+  fich << endl;
 }
--- obligatorio4/main.cpp
+++ obligatorio4/main.cpp
@@ -8,41 +8,27 @@
 
 #define N 4 // Número de ecuaciones diferenciales
 #define G 6.6738e-11 // Cte Gravitación Universal
+#define dTL 3.844e8  // Distancia Tierra-Luna (m)
+#define w 2.60146e-9 // vL = 1.0 (km/s)
 using namespace std;
 
 int main(){
   // Parámetros iniciales
-  int t0=10; // Nº máximo de iteraciones
-  float h=1, t=0; // h = 1min, tiempo
-  float y[N], k[4][N]; //Radio orbital, velocidad orbital, masa, aceleración, w y periodos
-  ofstream fich;
+  int t0=1000; // Nº máximo de iteraciones
+  float h=60, r=6.378e6, phi=30, v=11.2e3, theta=30; // h = 1min; Radio (m), Velocidad (m/s), Ángulos (º)
 
   //***********
+  float y[N], k[4][N], t=0; // Coordenadas reescaladas, k^i de RK; tiempo (s)
+  ofstream fich;
 
   //a=0, w=0, Lectura de datos iniciales y Reescalamiento
-  LeerData(y);
-  Rescaling(y);
+  LeerReescalarData(y,r,phi,v,theta);
 
   //Apertura de fichero
   fich.open("nave_espacial.dat");
+  ExportData(fich,y,t);
 
-  //1ª aceleración y ctes mov iniciales
-  New_a(r,m,a); 
-  New_L_E(r,v,m,L,E);
-
-  //1ª escritura de posiciones y ctes mov iniciales
-  for(int i=0; i<N; i++)
-    f_r << r[i][0] << ",   " << r[i][1] << endl;
-  f_r << endl;
-  f_ctes << 0 << "   " << E << "   " << L << endl;
-
-  
-  for(float t=0; t<1600; t +=h){
-    n++;
-
-
-    }
-  }
+  //for(float t=0; t<=t0; t +=h)
 
   fich.close();
   return 0;
