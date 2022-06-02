58e344aef4c4419919556f6b272a41c654c7487a
Jose Rosano <e.funtoimagine18@go.ugr.es>
2022-06-02 20:24:59 +0200

    6.2_Trabajo en portátil con N=30 y w,th funciones

--- voluntario1/functions.h
+++ voluntario1/functions.h
@@ -14,13 +14,15 @@ using namespace std;
 
 void CrearPatronesDEF(int P);
 void LeerPatronesDEF(int patrones[][N][N], int P);
-void Generar_w(float w[][N][N][N], int patrones[][N][N], int P);
-void Generar_theta(float theta[][N], float w[][N][N][N]);
 void InitAleat(int s[][N]);
 void InitPatronDeg(int s[][N], int patrones[][N][N], float degeneracion);
 void ExportData(ofstream &fich, int s[N][N]);
 
-float New_H(int s[][N], int n, int m, float theta[][N], float w[][N][N][N]);
+void Generate_a(float a[], int patrones[][N][N], int P);
+float w(int i, int j, int k, int l, float a[], int patrones[][N][N], int P);
+float theta(int i, int j, float a[], int patrones[][N][N], int P);
+float New_H(int s[][N], int n, int m, float a[], float theta[][N], float w[][N][N][N]);
+float New_m(int s[N][N], int patrones[][N][N], int P);
 
 // *************************
 // Leer Patrones Iniciales con díjitos juntos, los separa y los escribe en otro fichero
@@ -84,47 +86,6 @@ void LeerPatronesDEF(int patrones[][N][N], int P){
   }
 }
 
-// Generar Interacciones Neuronales
-void Generar_w(float w[][N][N][N], int patrones[][N][N], int P){
-  float a[P], aux=1.0/(N*N);
-
-  // Generar a^nu
-  for(int nu=0; nu<P; nu++){
-    a[nu]=0;
-    for(int i=0; i<N; i++)
-      for(int j=0; j<N; j++)
-        a[nu] += patrones[nu][i][j];
-    a[nu] *= aux;
-  }
-
-  //Generar w_ij,kl (interacciones neuronales)
-  for(int i=0; i<N; i++)
-    for(int j=0; j<N; j++)
-      for(int k=0; k<N; k++)
-        for(int l=0; l<N; l++){
-          w[i][j][k][l] = 0;
-
-          if(i!=k && j!=l){
-            for(int nu=0; nu<P; nu++)
-              w[i][j][k][l] += (patrones[nu][i][j]-a[nu])*(patrones[nu][k][l]-a[nu]);
-            w[i][j][k][l] *= aux;
-          }
-        }
-}
-
-// Generar Umbrales de disparo
-void Generar_theta(float theta[][N], float w[][N][N][N]){
-  for(int i=0; i<N; i++)
-    for(int j=0; j<N; j++){
-      theta[i][j] = 0;
-
-      for(int k=0; k<N; k++)
-        for(int l=0; l<N; l++)
-          theta[i][j] += w[i][j][k][l];
-      theta[i][j] *= 0.5;
-    }
-}
-
 // Inicializar Matriz a Configuración Desordenada
 void InitAleat(int s[][N]){
   srand(time(NULL));
@@ -161,13 +122,66 @@ void ExportData(ofstream &fich, int s[N][N]){
   fich << endl;
 }
 
+// ********
+// Función Media de los patrones
+void Generate_anu(float a[], int patrones[][N][N], int P){
+  float a[P], aux=1.0/(N*N);
+
+  // Generar a^nu
+  for(int nu=0; nu<P; nu++){
+    a[nu]=0;
+    for(int i=0; i<N; i++)
+      for(int j=0; j<N; j++)
+        a[nu] += patrones[nu][i][j];
+    a[nu] *= aux;
+  }
+}
+
+// Generar Interacciones Neuronales
+float w(int i, int j, int k, int l, float a[], int patrones[][N][N], int P){
+  float aux=1.0/(N*N), w_res=0;
+
+  //Generar w_ij,kl (interacciones neuronales)
+  if(i!=k && j!=l){
+    for(int nu=0; nu<P; nu++)
+      w_res += (patrones[nu][i][j]-a[nu])*(patrones[nu][k][l]-a[nu]);
+    w_res *= aux;
+  }
+  return w_res;
+}
+
+// Generar Umbrales de disparo
+float theta(int i, int j, float a[], int patrones[][N][N], int P){
+  float theta_res=0;
+
+  for(int k=0; k<N; k++)
+    for(int l=0; l<N; l++)
+      theta_res += w(i,j,k,l,a,patrones,P);
+  theta_res *= 0.5;
+  return theta_res;
+}
+
 // Función Delta(E)
-float New_H(int s[N][N], int n, int m, float theta[][N], float w[][N][N][N]){
+float New_H(int s[N][N], int n, int m, float a[], int patrones[][N][N], int P){
   float aux=0;
 
   for(int i=0; i<N; i++)
     for(int j=0; j<N-1; j++)
-      aux += w[n][m][i][j];
+      aux += w(n,m,i,j,a,patrones,P);
 
-  return (1-2*s[n][m])*(theta[n][m]-aux);
+  return (1-2*s[n][m])*(theta(n,m,a,patrones,P)-aux);
+}
+
+// Función Solapamiento
+void New_m(float m[], int s[N][N], float a[], int patrones[][N][N], int P){
+  float aux;
+
+  for(int nu=0; nu<N; nu++){
+    aux = N*N*a[nu]*(1-a[nu]);
+
+    for(int i=0; i<N; i++)
+      for(int j=0; j<N-1; j++)
+        m[nu] += (patrones[nu][i][j]-a[nu])*(s[i][j]-a[nu]);
+    m[nu] *= aux;
+  }
 }
